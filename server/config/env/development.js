0e5e2057ac976d5fbccd36245d383604ca9ba6d3
RaizmanLeonid <raizman_leonid@hotmail.com>
2014-07-16 17:51:15 +0300

    adding

---
+++ server/config/env/development.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+    db: 'mongodb://localhost/mean-dev',
+    app: {
+        name: 'postapp dev'
+    },
+    facebook: {
+        clientID: '641796912562834', //'APP_ID',
+        clientSecret: '52146ab13cce4ebb0c29c36312d3ff97', //'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'CONSUMER_KEY',
+        clientSecret: 'CONSUMER_SECRET',
+        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    },
+    github: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    linkedin: {
+        clientID: 'API_KEY',
+        clientSecret: 'SECRET_KEY',
+        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    }
+};
